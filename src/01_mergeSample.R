9dd2d75d2f49ffeec2b57e10f8e318189a6afa1f
cystone <cystone@Mac-mini.lan>
2021-03-30 23:44:38 +0800

    修改了文件结构

--- src/01_mergeSample.R
+++ src/01_mergeSample.R
@@ -3,11 +3,11 @@ library(tidyverse)
 library(patchwork)
 library(data.table)
 dir.create('./output/01_mergeSample/cluster1', recursive=T)
-dir.create('./output/01_mergeSample/cluster1', recursive=T)
-dir.create('./output/01_mergeSample/cluster1', recursive=T)
+dir.create('./output/01_mergeSample/cluster2', recursive=T)
+dir.create('./output/01_mergeSample/cluster3', recursive=T)
 set.seed(123)  #设置随机数种子，使结果可重复
 
-#---------样本合并-----------
+#----------读入数据----------
 ##使用目录向量合并
 dir = c('./data/raw/GSE139324_RAW/GSM4138110', 
         './data/raw/GSE139324_RAW/GSM4138111',
@@ -39,7 +39,7 @@ names(dir) = c('HNC01PBMC', 'HNC01TIL', 'HNC10PBMC', 'HNC10TIL', 'HNC20PBMC',
                'HNC20TIL', 'PBMC1', 'PBMC2', 'Tonsil1', 'Tonsil2')
 counts <- Read10X(data.dir = dir)
 
-
+#-------样本合并-------
 scRNA1 = CreateSeuratObject(counts, min.cells=1)
 dim(scRNA1)   #查看基因数和细胞总数
 #[1] 23603 19750 
@@ -55,73 +55,87 @@ for(i in 1:length(dir)){
   scRNAlist[[i]] <- CreateSeuratObject(counts, min.cells=1)
 }
 #使用merge函数讲10个seurat对象合并成一个seurat对象
-scRNA2 <- merge(scRNAlist[[1]], y=c(scRNAlist[[2]], scRNAlist[[3]], 
-                                    scRNAlist[[4]], scRNAlist[[5]], scRNAlist[[6]], scRNAlist[[7]], 
-                                    scRNAlist[[8]], scRNAlist[[9]], scRNAlist[[10]]))
+scRNA2 <- merge(scRNAlist[[1]],
+                y=c(scRNAlist[[2]], scRNAlist[[3]], scRNAlist[[4]],
+                    scRNAlist[[5]], scRNAlist[[6]], scRNAlist[[7]], 
+                    scRNAlist[[8]], scRNAlist[[9]], scRNAlist[[10]]))
 #dim(scRNA2)
 # [1] 23603 19750
 table(scRNA2@meta.data$orig.ident)
 #HNC01PBMC  HNC01TIL HNC10PBMC  HNC10TIL HNC20PBMC  HNC20TIL     PBMC1     PBMC2   Tonsil1   Tonsil2 
 #     1725      1298      1750      1384      1530      1148      2445      2436      3325      2709
 
+#计算主成分
+{
+  scRNA1 <- NormalizeData(scRNA1)
+  scRNA1 <- FindVariableFeatures(scRNA1, selection.method = "vst")
+  scRNA1 <- ScaleData(scRNA1, features = VariableFeatures(scRNA1))
+  scRNA1 <- RunPCA(scRNA1, features = VariableFeatures(scRNA1))
+  plot1 <- DimPlot(scRNA1, reduction = "pca", group.by="orig.ident")
+  plot2 <- ElbowPlot(scRNA1, ndims=30, reduction="pca") 
+  plotc <- plot1+plot2
+  ggsave("./output/01_mergeSample/cluster1/pca.png", plot = plotc, width = 8, 
+         height = 4)
+  print(c("请选择哪些pc轴用于后续分析？示例如下：","pc.num=1:15"))
+}
 
-scRNA1 <- NormalizeData(scRNA1)
-scRNA1 <- FindVariableFeatures(scRNA1, selection.method = "vst")
-scRNA1 <- ScaleData(scRNA1, features = VariableFeatures(scRNA1))
-scRNA1 <- RunPCA(scRNA1, features = VariableFeatures(scRNA1))
-plot1 <- DimPlot(scRNA1, reduction = "pca", group.by="orig.ident")
-plot2 <- ElbowPlot(scRNA1, ndims=30, reduction="pca") 
-plotc <- plot1+plot2
-ggsave("./output/01_mergeSample/cluster1/pca.png", plot = plotc, width = 8, height = 4)
-print(c("请选择哪些pc轴用于后续分析？示例如下：","pc.num=1:15"))
 #选取主成分
 pc.num=1:30
+# 降维和非线性聚类
+{
+  ##细胞聚类
+  scRNA1 <- FindNeighbors(scRNA1, dims = pc.num) 
+  scRNA1 <- FindClusters(scRNA1, resolution = 0.5)
+  table(scRNA1@meta.data$seurat_clusters)
+  metadata <- scRNA1@meta.data
+  cell_cluster <- data.frame(cell_ID=rownames(metadata), 
+                             cluster_ID=metadata$seurat_clusters)
+  write.csv(cell_cluster,'./output/01_mergeSample/cluster1/cell_cluster.csv',
+            row.names = F)
+  
+  ##非线性降维
+  #tSNE
+  scRNA1 = RunTSNE(scRNA1, dims = pc.num)
+  embed_tsne <- Embeddings(scRNA1, 'tsne')   #提取tsne图坐标
+  write.csv(embed_tsne,'./output/01_mergeSample/cluster1/embed_tsne.csv')
+  #group_by_cluster
+  plot1 = DimPlot(scRNA1, reduction = "tsne", label=T) 
+  ggsave("./output/01_mergeSample/cluster1/tSNE.png", plot = plot1, width = 8,
+         height = 7)
+  #group_by_sample
+  plot2 = DimPlot(scRNA1, reduction = "tsne", group.by='orig.ident') 
+  ggsave("./output/01_mergeSample/cluster1/tSNE_sample.png", plot = plot2, 
+         width = 8, height = 7)
+  #combinate
+  plotc <- plot1+plot2
+  ggsave("./output/01_mergeSample/cluster1/tSNE_cluster_sample.png", plot = plotc, 
+         width = 10, height = 5)
+  
+  #UMAP
+  scRNA1 <- RunUMAP(scRNA1, dims = pc.num)
+  embed_umap <- Embeddings(scRNA1, 'umap')   #提取umap图坐标
+  write.csv(embed_umap,'./output/01_mergeSample/cluster1/embed_umap.csv') 
+  #group_by_cluster
+  plot3 = DimPlot(scRNA1, reduction = "umap", label=T) 
+  ggsave("./output/01_mergeSample/cluster1/UMAP.png", plot = plot3, width = 8, 
+         height = 7)
+  #group_by_sample
+  plot4 = DimPlot(scRNA1, reduction = "umap", group.by='orig.ident')
+  ggsave("./output/01_mergeSample/cluster1/UMAP.png", plot = plot4, width = 8, 
+         height = 7)
+  #combinate
+  plotc <- plot3+plot4
+  ggsave("./output/01_mergeSample/cluster1/UMAP_cluster_sample.png", plot = plotc,
+         width = 10, height = 5)
+}
 
-##细胞聚类
-scRNA1 <- FindNeighbors(scRNA1, dims = pc.num) 
-scRNA1 <- FindClusters(scRNA1, resolution = 0.5)
-table(scRNA1@meta.data$seurat_clusters)
-metadata <- scRNA1@meta.data
-cell_cluster <- data.frame(cell_ID=rownames(metadata), cluster_ID=metadata$seurat_clusters)
-write.csv(cell_cluster,'./output/01_mergeSample/cluster1/cell_cluster.csv',row.names = F)
-
-##非线性降维
-#tSNE
-scRNA1 = RunTSNE(scRNA1, dims = pc.num)
-embed_tsne <- Embeddings(scRNA1, 'tsne')   #提取tsne图坐标
-write.csv(embed_tsne,'./output/01_mergeSample/cluster1/embed_tsne.csv')
-#group_by_cluster
-plot1 = DimPlot(scRNA1, reduction = "tsne", label=T) 
-ggsave("./output/01_mergeSample/cluster1/tSNE.png", plot = plot1, width = 8, height = 7)
-#group_by_sample
-plot2 = DimPlot(scRNA1, reduction = "tsne", group.by='orig.ident') 
-ggsave("./output/01_mergeSample/cluster1/tSNE_sample.png", plot = plot2, width = 8, height = 7)
-#combinate
-plotc <- plot1+plot2
-ggsave("./output/01_mergeSample/cluster1/tSNE_cluster_sample.png", plot = plotc, width = 10, height = 5)
-
-#UMAP
-scRNA1 <- RunUMAP(scRNA1, dims = pc.num)
-embed_umap <- Embeddings(scRNA1, 'umap')   #提取umap图坐标
-write.csv(embed_umap,'./output/01_mergeSample/cluster1/embed_umap.csv') 
-#group_by_cluster
-plot3 = DimPlot(scRNA1, reduction = "umap", label=T) 
-ggsave("./output/01_mergeSample/cluster1/UMAP.png", plot = plot3, width = 8, height = 7)
-#group_by_sample
-plot4 = DimPlot(scRNA1, reduction = "umap", group.by='orig.ident')
-ggsave("./output/01_mergeSample/cluster1/UMAP.png", plot = plot4, width = 8, height = 7)
-#combinate
-plotc <- plot3+plot4
-ggsave("./output/01_mergeSample/cluster1/UMAP_cluster_sample.png", plot = plotc, width = 10, height = 5)
 
 #合并tSNE与UMAP
 plotc <- plot2+plot4+ plot_layout(guides = 'collect')
-ggsave("./output/01_mergeSample/cluster1/tSNE_UMAP.png", plot = plotc, width = 10, height = 5)
+ggsave("./output/01_mergeSample/cluster1/tSNE_UMAP.png", plot = plotc, 
+       width = 10, height = 5)
 
 ##scRNA2对象的降维聚类参考scRNA1的代码
-
-
-
 #------------数据集合并---------------
 
 #scRNAlist是之前代码运行保存好的seurat对象列表，保存了10个样本的独立数据
@@ -140,7 +154,6 @@ dim(scRNA3)
 #降维聚类的代码省略
 
 #-----------数据质控------------
-##==数据质控==#
 scRNA <- scRNA3  #以后的分析使用整合的数据进行
 ##meta.data添加信息
 dir.create("./output/01_mergeSample/QC", recursive=T)
@@ -169,79 +182,185 @@ violin <-VlnPlot(scRNA, group.by = "proj_name",
                  cols =rainbow(col.num), 
                  pt.size = 0.01, #不需要显示点，可以设置pt.size = 0
                  ncol = 4) + 
-  theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) 
-ggsave("./output/01_mergeSample/QC/vlnplot_before_qc.pdf", plot = violin, width = 12, height = 6) 
-ggsave("./output/01_mergeSample/QC/vlnplot_before_qc.png", plot = violin, width = 12, height = 6)  
+  theme(axis.title.x=element_blank(), axis.text.x=element_blank(), 
+        axis.ticks.x=element_blank()) 
+ggsave("./output/01_mergeSample/QC/vlnplot_before_qc.pdf", plot = violin,
+       width = 12, height = 6) 
+ggsave("./output/01_mergeSample/QC/vlnplot_before_qc.png", plot = violin, 
+       width = 12, height = 6)  
 plot1 <- FeatureScatter(scRNA, feature1 = "nCount_RNA", feature2 = "percent.mt")
 plot2 <- FeatureScatter(scRNA, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 plot3 <- FeatureScatter(scRNA, feature1 = "nCount_RNA", feature2 = "percent.HB")
 pearplot <- CombinePlots(plots = list(plot1, plot2, plot3), nrow=1, legend="none") 
-ggsave("./output/01_mergeSample/QC/pearplot_before_qc.pdf", plot = pearplot, width = 12, height = 5) 
-ggsave("./output/01_mergeSample/QC/pearplot_before_qc.png", plot = pearplot, width = 12, height = 5)
+ggsave("./output/01_mergeSample/QC/pearplot_before_qc.pdf", plot = pearplot, 
+       width = 12, height = 5) 
+ggsave("./output/01_mergeSample/QC/pearplot_before_qc.png", plot = pearplot,
+       width = 12, height = 5)
 
 ##设置质控标准
-print(c("请输入允许基因数和核糖体比例，示例如下：", "minGene=500", "maxGene=4000", "pctMT=20"))
+print(c("请输入允许基因数和核糖体比例，示例如下：", "minGene=500",
+        "maxGene=4000", "pctMT=20"))
 minGene=500
 maxGene=3000
 pctMT=10
 
 ##数据质控
-scRNA <- subset(scRNA, subset = nFeature_RNA > minGene & nFeature_RNA < maxGene & percent.mt < pctMT)
+scRNA <- subset(scRNA, 
+                subset = nFeature_RNA > minGene & nFeature_RNA < maxGene & 
+                  percent.mt < pctMT)
 col.num <- length(levels(as.factor(scRNA@meta.data$orig.ident)))
 violin <-VlnPlot(scRNA, group.by = "proj_name",
-                 features = c("nFeature_RNA", "nCount_RNA", "percent.mt","percent.HB"), 
+                 features = c("nFeature_RNA", "nCount_RNA", 
+                              "percent.mt","percent.HB"), 
                  cols =rainbow(col.num), 
                  pt.size = 0.1, 
                  ncol = 4) + 
-  theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) 
-ggsave("./output/01_mergeSample/QC/vlnplot_after_qc.pdf", plot = violin, width = 12, height = 6) 
-ggsave("./output/01_mergeSample/QC/vlnplot_after_qc.png", plot = violin, width = 12, height = 6)
+  theme(axis.title.x=element_blank(), axis.text.x=element_blank(), 
+        axis.ticks.x=element_blank()) 
+ggsave("./output/01_mergeSample/QC/vlnplot_after_qc.pdf", plot = violin, 
+       width = 12, height = 6) 
+ggsave("./output/01_mergeSample/QC/vlnplot_after_qc.png", plot = violin, 
+       width = 12, height = 6)
 
 #-----------------细胞类型鉴定-------------------
 ##==鉴定细胞类型==##
 library(SingleR)
 dir.create("./output/01_mergeSample/CellType")
-refdata <- MonacoImmuneData()
+#计算主成分
+{
+  scRNA <- NormalizeData(scRNA)
+  scRNA <- FindVariableFeatures(scRNA, selection.method = "vst")
+  scRNA <- ScaleData(scRNA, features = VariableFeatures(scRNA))
+  scRNA <- RunPCA(scRNA, features = VariableFeatures(scRNA))
+  plot1 <- DimPlot(scRNA, reduction = "pca", group.by="orig.ident")
+  plot2 <- ElbowPlot(scRNA, ndims=30, reduction="pca") 
+  plotc <- plot1+plot2
+  ggsave("./output/01_mergeSample/cluster3/pca.png", plot = plotc, width = 8, 
+         height = 4)
+  print(c("请选择哪些pc轴用于后续分析？示例如下：","pc.num=1:15"))
+}
+
+#选取主成分
+pc.num=1:30
+# 降维和非线性聚类
+{
+  ##细胞聚类
+  scRNA <- FindNeighbors(scRNA, dims = pc.num) 
+  scRNA <- FindClusters(scRNA, resolution = 0.5)
+  table(scRNA@meta.data$seurat_clusters)
+  metadata <- scRNA@meta.data
+  cell_cluster <- data.frame(cell_ID=rownames(metadata), 
+                             cluster_ID=metadata$seurat_clusters)
+  write.csv(cell_cluster,'./output/01_mergeSample/cluster3/cell_cluster.csv',
+            row.names = F)
+  
+  ##非线性降维
+  #tSNE
+  scRNA = RunTSNE(scRNA, dims = pc.num)
+  embed_tsne <- Embeddings(scRNA, 'tsne')   #提取tsne图坐标
+  write.csv(embed_tsne,'./output/01_mergeSample/cluster3/embed_tsne.csv')
+  #group_by_cluster
+  plot1 = DimPlot(scRNA, reduction = "tsne", label=T) 
+  ggsave("./output/01_mergeSample/cluster3/tSNE.png", plot = plot1, width = 8,
+         height = 7)
+  #group_by_sample
+  plot2 = DimPlot(scRNA, reduction = "tsne", group.by='orig.ident') 
+  ggsave("./output/01_mergeSample/cluster3/tSNE_sample.png", plot = plot2, 
+         width = 8, height = 7)
+  #combinate
+  plotc <- plot1+plot2
+  ggsave("./output/01_mergeSample/cluster3/tSNE_cluster_sample.png", plot = plotc, 
+         width = 10, height = 5)
+  
+  #UMAP
+  scRNA <- RunUMAP(scRNA, dims = pc.num)
+  embed_umap <- Embeddings(scRNA1, 'umap')   #提取umap图坐标
+  write.csv(embed_umap,'./output/01_mergeSample/cluster3/embed_umap.csv') 
+  #group_by_cluster
+  plot3 = DimPlot(scRNA, reduction = "umap", label=T) 
+  ggsave("./output/01_mergeSample/cluster3/UMAP.png", plot = plot3, width = 8, 
+         height = 7)
+  #group_by_sample
+  plot4 = DimPlot(scRNA, reduction = "umap", group.by='orig.ident')
+  ggsave("./output/01_mergeSample/cluster3/UMAP.png", plot = plot4, width = 8, 
+         height = 7)
+  #combinate
+  plotc <- plot3+plot4
+  ggsave("./output/01_mergeSample/cluster3/UMAP_cluster_sample.png", plot = plotc,
+         width = 10, height = 5)
+}
+
+
+#合并tSNE与UMAP
+plotc <- plot2+plot4+ plot_layout(guides = 'collect')
+ggsave("./output/01_mergeSample/cluster3/tSNE_UMAP.png", plot = plotc, 
+       width = 10, height = 5)
+
+
+# refdata <- MonacoImmuneData()
+# save(refdata, file = '~/database/SingleR_ref/ref_MonacoImmuneData.RData')
+load(file = '~/database/SingleR_ref/ref_MonacoImmuneData.RData')
 testdata <- GetAssayData(scRNA, slot="data")
 clusters <- scRNA@meta.data$seurat_clusters
 #使用Monaco参考数据库鉴定
 cellpred <- SingleR(test = testdata, ref = refdata, labels = refdata$label.main, 
                     method = "cluster", clusters = clusters, 
                     assay.type.test = "logcounts", assay.type.ref = "logcounts")
-celltype = data.frame(ClusterID=rownames(cellpred), celltype=cellpred$labels, stringsAsFactors = F)
-write.csv(celltype,"./output/01_mergeSample/CellType/celltype_Monaco.csv",row.names = F)
+celltype = data.frame(ClusterID=rownames(cellpred), 
+                      celltype=cellpred$labels, stringsAsFactors = F)
+write.csv(celltype,"./output/01_mergeSample/CellType/celltype_Monaco.csv",
+          row.names = F)
 scRNA@meta.data$celltype_Monaco = "NA"
 for(i in 1:nrow(celltype)){
-  scRNA@meta.data[which(scRNA@meta.data$seurat_clusters == celltype$ClusterID[i]),'celltype_Monaco'] <- celltype$celltype[i]}
-p1 = DimPlot(scRNA, group.by="celltype_Monaco", repel=T, label=T, label.size=5, reduction='tsne')
-p2 = DimPlot(scRNA, group.by="celltype_Monaco", repel=T, label=T, label.size=5, reduction='umap')
+  ind = which(scRNA@meta.data$seurat_clusters == celltype$ClusterID[i])
+  scRNA@meta.data[ind,'celltype_Monaco'] <- celltype$celltype[i]}
+p1 = DimPlot(scRNA, group.by="celltype_Monaco", repel=T, label=T, label.size=5,
+             reduction='tsne')
+p2 = DimPlot(scRNA, group.by="celltype_Monaco", repel=T, label=T, label.size=5,
+             reduction='umap')
 p3 = p1+p2+ plot_layout(guides = 'collect')
-ggsave("./output/01_mergeSample/CellType/tSNE_celltype_Monaco.png", p1, width=7 ,height=6)
-ggsave("./output/01_mergeSample/CellType/UMAP_celltype_Monaco.png", p2, width=7 ,height=6)
-ggsave("./output/01_mergeSample/CellType/celltype_Monaco.png", p3, width=10 ,height=5)
+ggsave("./output/01_mergeSample/CellType/tSNE_celltype_Monaco.png", p1, 
+       width=7 ,height=6)
+ggsave("./output/01_mergeSample/CellType/UMAP_celltype_Monaco.png", p2, 
+       width=7 ,height=6)
+ggsave("./output/01_mergeSample/CellType/celltype_Monaco.png", p3, width=10 ,
+       height=5)
 #使用DICE参考数据库鉴定
-refdata <- DatabaseImmuneCellExpressionData()
-# load('~/database/SingleR_ref/ref_DICE_1561s.RData')
+# refdata <- DatabaseImmuneCellExpressionData()
+# save(refdata, file='~/database/SingleR_ref/ref_DICE_1561s.RData')
+load('~/database/SingleR_ref/ref_DICE_1561s.RData')
 # refdata <- ref_DICE
 testdata <- GetAssayData(scRNA, slot="data")
 clusters <- scRNA@meta.data$seurat_clusters
+#tSNE
+scRNA = RunTSNE(scRNA, dims = pc.num)
+embed_tsne <- Embeddings(scRNA, 'tsne') 
+
 cellpred <- SingleR(test = testdata, ref = refdata, labels = refdata$label.main, 
                     method = "cluster", clusters = clusters, 
                     assay.type.test = "logcounts", assay.type.ref = "logcounts")
-celltype = data.frame(ClusterID=rownames(cellpred), celltype=cellpred$labels, stringsAsFactors = F)
-write.csv(celltype,"./output/01_mergeSample/CellType/celltype_DICE.csv",row.names = F)
+celltype = data.frame(ClusterID=rownames(cellpred), celltype=cellpred$labels, 
+                      stringsAsFactors = F)
+write.csv(celltype,"./output/01_mergeSample/CellType/celltype_DICE.csv",
+          row.names = F)
 scRNA@meta.data$celltype_DICE = "NA"
 for(i in 1:nrow(celltype)){
-  scRNA@meta.data[which(scRNA@meta.data$seurat_clusters == celltype$ClusterID[i]),'celltype_DICE'] <- celltype$celltype[i]}
-p4 = DimPlot(scRNA, group.by="celltype_DICE", repel=T, label=T, label.size=5, reduction='tsne')
-p5 = DimPlot(scRNA, group.by="celltype_DICE", repel=T, label=T, label.size=5, reduction='umap')
+  ind = which(scRNA@meta.data$seurat_clusters == celltype$ClusterID[i])
+  scRNA@meta.data[ind,'celltype_DICE'] <- celltype$celltype[i]}
+p4 = DimPlot(scRNA, group.by="celltype_DICE", repel=T, label=T, label.size=5, 
+             reduction='tsne')
+p5 = DimPlot(scRNA, group.by="celltype_DICE", repel=T, label=T, label.size=5, 
+             reduction='umap')
 p6 = p3+p4+ plot_layout(guides = 'collect')
-ggsave("./output/01_mergeSample/CellType/tSNE_celltype_DICE.png", p4, width=7 ,height=6)
-ggsave("./output/01_mergeSample/CellType/UMAP_celltype_DICE.png", p5, width=7 ,height=6)
-ggsave("./output/01_mergeSample/CellType/celltype_DICE.png", p6, width=10 ,height=5)
+ggsave("./output/01_mergeSample/CellType/tSNE_celltype_DICE.png", p4, width=7,
+       height=6)
+ggsave("./output/01_mergeSample/CellType/UMAP_celltype_DICE.png", p5, width=7,
+       height=6)
+ggsave("./output/01_mergeSample/CellType/celltype_DICE.png", p6, width=10,
+       height=5)
 #对比两种数据库鉴定的结果
 p8 = p1+p4
-ggsave("./output/01_mergeSample/CellType/Monaco_DICE.png", p8, width=12 ,height=5)
+ggsave("./output/01_mergeSample/CellType/Monaco_DICE.png", p8, width=12,height=5)
 
 ##保存数据
 saveRDS(scRNA,'./output/01_mergeSample/scRNA.rds')
